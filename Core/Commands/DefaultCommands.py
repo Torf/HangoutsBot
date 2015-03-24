8d95fed3f29d8d6ae9093efabe144a736b92147c
Torf <tberger.dev@gmail.com>
2015-03-25 00:19:17 +0100

    Update DefaultCommands.py

--- Core/Commands/DefaultCommands.py
+++ Core/Commands/DefaultCommands.py
@@ -29,7 +29,7 @@ def think(bot, event, *args):
         for arg in args:
             if arg not in bot.config['autoreplies'][0][0]:
                 cleanargs.append(arg)
-        bot.send_message(event.conv, clever_session.think(' '.join(cleanargs)))
+        yield from bot.send_message(event.conv, clever_session.think(' '.join(cleanargs)))
 
 
 @DispatcherSingleton.register

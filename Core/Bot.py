fe65774908fe24fabbf6cc77aaa5a92de582177d
Torf <tberger.dev@gmail.com>
2015-03-26 23:58:26 +0100

    Update Bot.py

--- Core/Bot.py
+++ Core/Bot.py
@@ -242,7 +242,7 @@ class HangoutsBot(object):
                 file.write(text + '\n')
     
     def send_typing(self, conversation):
-      self._client.settyping(conv.id_)
+      self._client.settyping(conversation.id_)
     
     def send_message(self, conversation, text):
         """"Send simple chat message"""

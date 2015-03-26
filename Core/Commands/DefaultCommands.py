dae55391dadf5109ab51bdbf081bc5e0b48ef1ba
Torf <tberger.dev@gmail.com>
2015-03-26 22:34:22 +0100

    Update DefaultCommands.py

--- Core/Commands/DefaultCommands.py
+++ Core/Commands/DefaultCommands.py
@@ -14,7 +14,6 @@ from Core.Commands.Dispatcher import DispatcherSingleton
 from Core.Util import UtilBot
 
 last_recorded, last_recorder = None, None
-last_answer = {}
 
 @DispatcherSingleton.register_unknown
 def unknown_command(bot, event, *args):
@@ -24,44 +23,6 @@ def unknown_command(bot, event, *args):
 @DispatcherSingleton.register_hidden
 def me(bot, event, *args):
     pass
-
-@DispatcherSingleton.register_hidden
-def think(bot, event, *args):
-    if bot.chatterbot:
-        inputmsg = ' '.join(args)
-        answer = bot.chatterbot.think(inputmsg)
-        
-        last_answer[event.user_id] = event.timestamp
-        yield from bot.send_message(event.conv, answer)
-        
-@DispatcherSingleton.register_hidden
-def cleanthink(bot, event, *args):
-    if bot.chatterbot:
-        cleanargs = []
-        for arg in args:
-            if arg.lower() != bot.config['autoreplies_name']:
-                cleanargs.append(arg)
-        
-        yield from think(bot, event, *cleanargs)
-        
-@DispatcherSingleton.register_hidden
-def continuethink(bot, event, *args):
-    if bot.chatterbot:
-        if event.user_id in last_answer:
-            if last_answer[event.user_id] == event.timestamp:
-                for arg in args:
-                    if arg.lower() == bot.config['autoreplies_name']:
-                        return #already handled, TODO: enhance it
-            
-            diff = last_answer[event.user_id] - event.timestamp
-            if diff.total_seconds() < 120:
-                yield from think(bot, event, *args)
-
-@DispatcherSingleton.register_hidden
-def stopthink(bot, event, *args):
-    if bot.chatterbot:
-        if event.user_id in last_answer:
-            del last_answer[event.user_id]
 
 @DispatcherSingleton.register
 def help(bot, event, command=None, *args):

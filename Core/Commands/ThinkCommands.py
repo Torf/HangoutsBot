194a0e3309f876d37e841fe1c720d928c226a37f
Torf <tberger.dev@gmail.com>
2015-03-26 22:55:19 +0100

    Update ThinkCommands.py
    enhance think method

--- Core/Commands/ThinkCommands.py
+++ Core/Commands/ThinkCommands.py
@@ -14,38 +14,66 @@ from Core.Commands.Dispatcher import DispatcherSingleton
 from Core.Util import UtilBot
 
 last_answer = {}
+transtable = string.maketrans("","")
 
 @DispatcherSingleton.register_hidden
 def think(bot, event, *args):
-    if bot.chatterbot:
+    if bot.chatterbot and len(args) > 0:
         inputmsg = ' '.join(args)
-        answer = bot.chatterbot.think(inputmsg)
         
-        last_answer[event.user_id] = event.timestamp
-        yield from bot.send_message(event.conv, answer)
+        if wasSpeakingToBot(bot, event, *args, inputmsg):
+            answer(bot, event, inputmsg)
         
-@DispatcherSingleton.register_hidden
-def cleanthink(bot, event, *args):
-    if bot.chatterbot:
-        cleanargs = []
-        for arg in args:
-            if arg.lower() != bot.config['autoreplies_name']:
-                cleanargs.append(arg)
-        
-        yield from think(bot, event, *cleanargs)
-        
-@DispatcherSingleton.register_hidden
-def continuethink(bot, event, *args):
-    if bot.chatterbot:
-        if event.user_id in last_answer:
-            if last_answer[event.user_id] == event.timestamp:
-                for arg in args:
-                    if arg.lower() == bot.config['autoreplies_name']:
-                        return #already handled, TODO: enhance it
+        elif isSpeakingToBot(bot, *args, inputmsg):
+            answer(bot, event, inputmsg)
+
+
+def wasSpeakingToBot(bot, event, *args, inputmsg):
+    if event.user_id in last_answer and last_answer[event.user_id]:
+        diff = last_answer[event.user_id] - event.timestamp
+        if diff.total_seconds() => 0 and diff.total_seconds() <= 480:
+            return True
+        else:
+            del last_answer[event.user_id]
+    
+    return False
+
+
+def isSpeakingToBot(bot, *args, inputmsg):
+    botName = bot.config['autoreplies_name'].lower()
+    firstWord = args[0].lower()
+    
+    # @someone blabla
+    if firstWord.startswith('@'): 
+        if firstWord.startswith('@'+botName):
+            return True
+        else:
+            return False
             
-            diff = last_answer[event.user_id] - event.timestamp
-            if diff.total_seconds() < 120:
-                yield from think(bot, event, *args)
+    # Someone, blabla
+    if firstWord.startswith(botName):
+        return True
+    
+    # blabla Someone!
+    cleanmsg = inputmsg.lower()
+    if botName not in cleanmsg:
+        return False
+    
+    cleanmsg = cleanmsg.translate(transtable, string.punctuation).strip()
+    if cleanmsg.endswith(botName):
+        return True
+    
+    return False
+
+
+def answer(bot, event, inputmsg):
+    answer = bot.chatterbot.think(inputmsg)
+    last_answer[event.user_id] = event.timestamp
+    yield from bot.send_message(event.conv, answer)
+
+@DispatcherSingleton.register_hidden
+def taggle(bot, event, *args):
+    stopthink(bot, event, *args)
 
 @DispatcherSingleton.register_hidden
 def stopthink(bot, event, *args):

3fd07ae0cbce8c07607b60b3e6e3a62ec26f5432
Torf <tberger.dev@gmail.com>
2015-03-27 00:35:47 +0100

    Update ThinkCommands.py

--- Core/Commands/ThinkCommands.py
+++ Core/Commands/ThinkCommands.py
@@ -35,7 +35,7 @@ def think(bot, event, *args):
 def wasSpeakingToBot(event):
     if event.user_id.gaia_id in last_answer and last_answer[event.user_id.gaia_id]:
         print('in!')
-        diff = last_answer[event.user_id.gaia_id] - event.timestamp
+        diff = event.timestamp - last_answer[event.user_id.gaia_id]
         print('diff:%s'%diff)
         if diff.total_seconds() >= 0 and diff.total_seconds() <= 480:
             return True

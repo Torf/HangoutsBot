45a10c040c3b36c99787e3648a5c3bd60c7e3585
Torf <tberger.dev@gmail.com>
2015-03-27 01:05:16 +0100

    Update ThinkCommands.py

--- Core/Commands/ThinkCommands.py
+++ Core/Commands/ThinkCommands.py
@@ -38,7 +38,7 @@ def wasSpeakingToBot(event):
     if event.user_id.gaia_id in last_answer and last_answer[event.user_id.gaia_id]:
         diff = event.timestamp - last_answer[event.user_id.gaia_id]
         
-        if diff.total_seconds() >= 0 and diff.total_seconds() <= 480:
+        if diff.total_seconds() >= 0 and diff.total_seconds() <= bot.config['autoreplies_maxtime']:
             return True
         else:
             del last_answer[event.user_id.gaia_id]
